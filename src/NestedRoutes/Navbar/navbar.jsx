3ee20a3bf8e29cb2034c7166aacf15dfa37d2ebd
T-dev98 <133159152+T-dev98@users.noreply.github.com>
2024-02-13 14:54:45 -0500

    added link component in navbar

---
+++ src/NestedRoutes/Navbar/navbar.jsx
@@ -0,0 +1,15 @@
+import { Link, Outlet } from "react-router-dom";
+
+export default function Navbar(){
+    return(
+        <div>
+        <nav className="navbar" >
+            <Link to='/' >Home</Link>
+            <Link to='/about' >About</Link>
+            <Link to='/product' >Product</Link>
+            <Link to='/contact' >Contact</Link>
+        </nav>
+        <Outlet />
+        </div>
+    )
+}
